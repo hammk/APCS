a9bc80788e2e278d41e393bee95b370e28a5ea6b
Hamm Khan <humzahkha0143@k12.ipsd.org>
2022-09-16 09:55:33 -0500

    day two edition

--- APCompSci/src/Time.java
+++ APCompSci/src/Time.java
@@ -1,24 +1,77 @@
 /**
- * @author humzahkha0143
- * incomplete time checker
+  @author humzahkha0143
+  An incomplete time checker and setter for:
+  hours (getHours, setHours)
+  minutes (getMinutes, setMinutes)
+  seconds (getSeconds, setSeconds.)
  */
-
-package unit3;
 
 public class Time{
 	
-	public Time() {};                                       // default constructor
-	public Time(int cHours, int cMinutes, int cSeconds) {   // constructor with params
+	/**
+	  Initialize instance variables.
+	 */
+	
+	private int hours;
+	private int minutes;
+	private int seconds;
+	
+	/**
+	  Default constructor to define instance vars. No params, so set hours, minutes, and seconds to default.
+	 */
+	public Time() {
 		
-		cHours = hours;
-		cMinutes = minutes;
-		cSeconds = seconds;
+		hours = 12;
+		minutes = 34;
+		seconds = 56;
 		
 	};
 	
+	/**
+	 * Constructor with params to define instance vars hours, minutes, and seconds.
+	 * @param cHours 
+	 * @param cMinutes
+	 * @param cSeconds
+	 */
+	public Time(int cHours, int cMinutes, int cSeconds) {
+		
+		hours = cHours;
+		minutes = cMinutes;
+		seconds = cSeconds;
+		
+	};
+	
+	
+	/**
+	 * The following three do the same thing, except for different instance vars.
+	 * @return hours
+	 * @return minutes
+	 * @return seconds
+	 */
+	public int getHours() {
+		return hours;
+	}
+	public int getMinutes() {	
+		return minutes;
+	}
+	public int getSeconds() {
+		return seconds;
+	}
+	
+	/**
+	 * The following three intake a new int value for hours, minutes, and seconds.
+	 * @param nHours
+	 * @param nMins
+	 * @param nSeconds
+	 */
+	public void setHours(int nHours) {
+		hours = nHours;
+	}
+	public void setMinutes(int nMins) {
+		minutes = nMins;
+	}
+	public void setSeconds(int nSeconds) {
+		seconds = nSeconds;
+	}
+	
 };
-//public static void main(String args[]) {
-	
-	
-	
-//}
