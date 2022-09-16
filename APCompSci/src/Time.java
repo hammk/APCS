e00689b6ed36d37ebddbc0fa868d7687f2b7f1e9
Hamm Khan <humzahkha0143@k12.ipsd.org>
2022-09-16 10:04:07 -0500

    a

--- APCompSci/src/Time.java
+++ APCompSci/src/Time.java
@@ -1,30 +1,28 @@
 /**
   @author humzahkha0143
-  An incomplete time checker and setter for:
-  hours (getHours, setHours)
-  minutes (getMinutes, setMinutes)
-  seconds (getSeconds, setSeconds.)
+  An incomplete time checker and setter for: 
+  hours (getHours, setHours), 
+  minutes (getMinutes, setMinutes), 
+  and seconds (getSeconds, setSeconds.)
  */
 
 public class Time{
 	
 	/**
 	  Initialize instance variables.
-	 */
-	
+	*/
 	private int hours;
 	private int minutes;
 	private int seconds;
+	
 	
 	/**
 	  Default constructor to define instance vars. No params, so set hours, minutes, and seconds to default.
 	 */
 	public Time() {
-		
 		hours = 12;
 		minutes = 34;
 		seconds = 56;
-		
 	};
 	
 	/**
@@ -34,42 +32,62 @@ public class Time{
 	 * @param cSeconds
 	 */
 	public Time(int cHours, int cMinutes, int cSeconds) {
-		
 		hours = cHours;
 		minutes = cMinutes;
 		seconds = cSeconds;
-		
 	};
 	
+	/**
+	 * so begins day two...
+	 */
 	
 	/**
-	 * The following three do the same thing, except for different instance vars.
+	 * The following three 'get var' methods will do the same thing, except for different instance vars.
+	 * This one will get the 'hours' variable.
 	 * @return hours
-	 * @return minutes
-	 * @return seconds
 	 */
 	public int getHours() {
 		return hours;
 	}
+	
+	/**
+	 * The second of the three 'get var' methods.
+	 * This one will get the 'minutes' variable.
+	 * @return minutes
+	 */
 	public int getMinutes() {	
 		return minutes;
 	}
+	
+	/**
+	 * The third of the three 'get var' methods.
+	 * This one will get the 'seconds' variable.
+	 * @return seconds
+	 */
 	public int getSeconds() {
 		return seconds;
 	}
 	
 	/**
-	 * The following three intake a new int value for hours, minutes, and seconds.
+	 * The following three intake a new int value to define hours.
 	 * @param nHours
-	 * @param nMins
-	 * @param nSeconds
 	 */
 	public void setHours(int nHours) {
 		hours = nHours;
 	}
+	
+	/**
+	 * The second of the three 'set var' methods for defining minutes.
+	 * @param nMins
+	 */
 	public void setMinutes(int nMins) {
 		minutes = nMins;
 	}
+	
+	/**
+	 * The third of the three 'set var' methods for defining seconds.
+	 * @param nSeconds
+	 */
 	public void setSeconds(int nSeconds) {
 		seconds = nSeconds;
 	}
