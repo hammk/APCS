cf161b3cc2e423912f3289c09c453229faa79861
hammk <humzahkha0143@k12.ipsd.org>
2022-09-23 09:38:02 -0500

    asd

---
+++ APCompSci/src/rationalNumber/RationalNumber.java
@@ -0,0 +1,100 @@
+package rationalNumber;
+
+/**
+ * 
+ * @author humzahkha0143
+ *
+ */
+
+public class RationalNumber {
+	
+	private int numer;
+	private int denom;
+	
+	public RationalNumber() {
+		numer = 0;
+		denom = 0;
+	}
+	public RationalNumber(int nN, int nD) {
+		numer = nN;
+		denom = nD;
+	}
+
+	/**
+	 * Whatever calls this method will be returned the numerator stored in the instance fields.
+	 * @return numer - The numerator.
+	 */
+	public int getNumerator() {
+		return numer;
+	}
+	/**
+	 * Whatever calls this method will be returned the denominator stored in the instance fields.
+	 * @return denom - The denominator
+	 */
+	public int getDenominator() {
+		return denom;
+	}
+	
+	/**
+	 * Sets instance var 'numer' to parameter 'numer'.
+	 * @param numer - The replacement numerator
+	 */
+	public void setNumerator(int numer) {
+		this.numer = numer;
+	}
+	/**
+	 * Sets instance var 'denom' to parameter 'denom'.
+	 * @param denom - The replacement denominator
+	 */
+	public void setDenominator(int denom) {
+		this.denom = denom;
+	}
+	
+	/**
+	 * This will multiply the numerator and denominator stored in the instance vars and return it.
+	 * @return numer * denom
+	 */
+	public int getProduct() {
+		int prd = numer * denom;
+		return prd;
+	}
+	/**
+	 * This will multiply a given numerator and denominator and return it, as opposed to using the instance vars.
+	 * @param nN - Numerator
+	 * @param nD - Denominator
+	 * @return nN * nD
+	 */
+	public int getProduct(int nN, int nD) {
+		int prd = nN * nD;
+		return prd;
+	}
+	
+	/**
+	 * This will divide the numerator and denominator stored in the instance vars and return it;
+	 * @return numer / denom
+	 */
+	public double divideBy() {
+		int prd = numer / denom;
+		return prd;
+	}
+	/**
+	 * This will divide a given numerator and denominator and return it, as opposed to using the instance vars.
+	 * @param nN
+	 * @param nD
+	 * @return
+	 */
+	public double divideBy(int nN, int nD) {
+		int prd = nN / nD;
+		return prd;
+	}
+	
+	/**
+	 * Given that instance vars numer and denom have a value, they will be output to resemble a fraction.
+	 * @return 0/0
+	 */
+	public String toString() {
+		return numer + "/" + denom;
+	}
+	
+	
+}
