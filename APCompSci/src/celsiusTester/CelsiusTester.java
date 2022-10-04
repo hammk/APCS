5172cd9a31a4b41664d710c814c3871d50909119
hammk <humzahkha0143@k12.ipsd.org>
2022-10-04 09:47:28 -0500

    Final touches

--- APCompSci/src/celsiusTester/CelsiusTester.java
+++ APCompSci/src/celsiusTester/CelsiusTester.java
@@ -7,11 +7,11 @@ public class CelsiusTester {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);               // Cast scanner into an object 
-		System.out.print("Give me a number in Celsius: "); // Message for user to input..
+		System.out.print("Enter the Celsius temperature you want converted: "); // Message for user to input..
 		float num = sc.nextFloat();	                       // ..a float to store into num and to use in Celsius.java
 		
 		Celsius cs = new Celsius(num);                     // Cast our beloved Celsius.java into an object...
-		System.out.print(cs.convertCToF());                // Finally, take num and convert it from Celsius to Farenheit, and print the result.
+		System.out.print("The temperature " + num + " degrees Celsius is " + cs.convertCToF() + " degrees Farenheit.");                // Finally, take num and convert it from Celsius to Farenheit, and print the result.
 		
 		sc.close();
 
