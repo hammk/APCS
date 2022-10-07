4c5220a73108df1eb6de922620e7e1dede9742ea
hammk <humzahkha0143@k12.ipsd.org>
2022-10-07 09:30:32 -0500

    Make skeleton for balloon (very spooky!)

--- APCompSci/src/baloon/Balloon.java
+++ APCompSci/src/baloon/Balloon.java
@@ -8,5 +8,28 @@ public class Balloon {
 		vol = 1;
 	}
 	
+	public void addAir(int add) {
+		vol += add;
+		
+		
+	}
+	
+	public double getSurfaceArea() {
+		
+		
+		return 5;
+	}
+	
+	public double getVolume() {
+		
+		
+		return 5;
+	}
+	
+	public double getRadius() {
+		
+		
+		return 5;
+	}
 	
 }
