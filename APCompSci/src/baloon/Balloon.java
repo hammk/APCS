bf2cf38c9287c4da674b8e7c1eec3cfcec56fc40
hammk <67597830+hammk@users.noreply.github.com>
2022-10-07 10:05:08 -0500

    Javadocing.

--- APCompSci/src/baloon/Balloon.java
+++ APCompSci/src/baloon/Balloon.java
@@ -1,29 +1,51 @@
 package baloon;
 
+/**
+ * Defines a balloon object. Its only instance variable, vol, starts off deflated but can be blown into using the addAir method.
+ * By @author humzahkha0143.
+ */
+
 public class Balloon {
 
 	private int vol;
 	
+	/**
+	 * Default constructor; no params. The Balloon will start at a vol of 0 because it is deflated.
+	 */
 	public Balloon() {
 		vol = 0;
 	}
 	
+	/**
+	 * Blows air into the balloon, inflating it. It pops at a volume of 2,147,483,647.
+	 * @param add - Amount of air to blow into the balloon.
+	 */
 	public void addAir(int add) {
 		vol += add;
 	}
 	
+	/**
+	 * Returns the instance var representing volume.
+	 * @return vol
+	 */
 	public double getVolume() {
 		return vol;
 	}
 	
+	/**
+	 * 
+	 * @return 
+	 */
 	public double getSurfaceArea() {
-		// 4 * Math.PI * rad^2
 		double rad = Math.cbrt(vol / ( (4/3) / Math.PI) );
 		return (Math.pow(rad, 2) * Math.PI * 4);
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public double getRadius() {
-		// volume = (4/3) * Math.PI * r3
 		double r3 = vol / (4/3) / Math.PI;
 		return Math.cbrt(r3);
 	}
