223c292188527b7c850539ee8a7562ea54e7ccd0
hammk <67597830+hammk@users.noreply.github.com>
2022-11-04 09:42:17 -0500

    Comments added to tester

--- APCompSci/src/rps/RPSTest.java
+++ APCompSci/src/rps/RPSTest.java
@@ -1,3 +1,4 @@
+// by humzahkha0143
 package rps;
 
 import java.util.Scanner;
@@ -6,8 +7,10 @@ public class RPSTest {
 
     public static void main(String[] args) {
         
+        // Instantiate choice and initialize String playerChoice
         Scanner choice = new Scanner(System.in);
         String pC;
+        // Begin Do-While loop, it's guaranteed to run at least once.
         do {
             System.out.print("Please enter Rock, Paper, or Scissors: ");
             pC = choice.next();
@@ -16,6 +19,7 @@ public class RPSTest {
             pC = choice.next();
         }
         while(!pC.equalsIgnoreCase("yes"));
+        // Inevitable condition fail (the user wants out)
         System.out.println("C YA!");
         choice.close();
     }
