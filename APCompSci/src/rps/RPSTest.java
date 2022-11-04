30c98dc0e376495260cbb2f2864aecd34fb8efde
hamm <67597830+hammk@users.noreply.github.com>
2022-11-04 09:44:47 -0500

    Completed part 2 of RPS Lab (added loop to gameplay, sentinel for quitting, tester comments)

--- APCompSci/src/rps/RPSTest.java
+++ APCompSci/src/rps/RPSTest.java
@@ -1,3 +1,4 @@
+// by humzahkha0143
 package rps;
 
 import java.util.Scanner;
@@ -6,12 +7,21 @@ public class RPSTest {
 
     public static void main(String[] args) {
         
+        // Instantiate choice and initialize String playerChoice
         Scanner choice = new Scanner(System.in);
-        System.out.print("Please enter Rock, Paper, or Scissors: ");
-        String pC = choice.next();
-        System.out.println(RPS.playRPS(pC));
+        String pC;
+        // Begin Do-While loop, it's guaranteed to run at least once.
+        do {
+            System.out.print("Please enter Rock, Paper, or Scissors: ");
+            pC = choice.next();
+            System.out.println(RPS.playRPS(pC));
+            System.out.print("\nFinished playing? If so, enter yes. ");
+            pC = choice.next();
+        }
+        while(!pC.equalsIgnoreCase("yes"));
+        // Inevitable condition fail (the user wants out)
+        System.out.println("C YA!");
         choice.close();
-        
     }
 
 }
