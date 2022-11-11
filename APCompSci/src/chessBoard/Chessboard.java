caa87a75c9eaa9fea0f4aa3c088886c0e12755e9
hammk <67597830+hammk@users.noreply.github.com>
2022-11-11 10:36:58 -0600

    Chess progress

--- APCompSci/src/chessBoard/Chessboard.java
+++ APCompSci/src/chessBoard/Chessboard.java
@@ -1,5 +1,6 @@
 package chessBoard;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Chessboard {
@@ -14,9 +15,24 @@ public class Chessboard {
 
     public void draw(Graphics2D g2) {
         
-        int count, y = 0;
-        for(count = 0; y <= sz; count += sq) {
-           if(count <= sz) g2.drawRect(count, y, sz, sz); 
+        int count, y;
+        boolean wr = true;
+        for(y = 0; y <= sz; y += sz) {
+            
+           for(count = 0; count <= sz; count += sz) {
+               
+               if(wr == true) {
+                   g2.setColor(Color.red);
+                   wr = false;
+               }
+               else {
+                   g2.setColor(Color.white);
+                   wr = true;
+               }
+               g2.drawRect(count, y, sz, sz); 
+               
+           }
+                   
         }
         
         
