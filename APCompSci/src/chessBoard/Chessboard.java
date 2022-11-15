75fb1fffa0113705b4afb0d57348be079201ea17
hamm <67597830+hammk@users.noreply.github.com>
2022-11-14 21:08:16 -0600

    Interest project complete

--- APCompSci/src/chessBoard/Chessboard.java
+++ APCompSci/src/chessBoard/Chessboard.java
@@ -2,24 +2,42 @@ package chessBoard;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+/**
+ * Class to be used by JComponents.
+ */
 
 public class Chessboard {
 
     private int sq;
     private int sz;
     
+    /**
+     * Default constructor, two parameters
+     * @param Squares - Amount of Squares on the checkerboard.
+     * @param Size - How big the squares should be on the checkerboard.
+     */
     public Chessboard (int Squares, int Size) {
         sq = Squares;
         sz = Size;
     }
 
+    /**
+     * Method calls a loop that increments the following square's y by
+     * the size of the square until the amount of squares on that axis
+     * achieved. Inside is another loop that increments the following is
+     * square's x by the size of the square until it reaches a threshold
+     * of the amount of squares multiplied by how big the squares are,
+     * meanwhile flipfloppping between colors to give the squares.
+     */
     public void draw(Graphics2D g2) {
         
-        int count, y;
+        int x, y;
         boolean wr = true;
-        for(y = 0; y <= sq; y += sq) {
+
+        
+        for(y = 0; y <= (sq * sz); y+=sz) {
             
-           for(count = 0; count <= sz; count += sz) {
+           for(x = 0; x <= (sq * sz); x+=sz) {
                
                if(wr == true) {
                    g2.setColor(Color.red);
@@ -29,7 +47,7 @@ public class Chessboard {
                    g2.setColor(Color.white);
                    wr = true;
                }
-               g2.drawRect(count, y, sz, sz); 
+               g2.fillRect(x, y, sz, sz); 
                
            }
                    
--- APCompSci/src/interest/Interest.java
+++ APCompSci/src/interest/Interest.java
@@ -2,25 +2,42 @@ package interest;
 
 public class Interest {
     
-    private int cash;
+    private double cash;
     private int rate;
 
+    /**
+     * Default constructor - two parameters.
+     * @param c - Starting Balance
+     * @param r - Interest Rate
+     */
     public Interest (int c, int r) {
         cash = c;
         rate = r;
     }
 
-
+    /**
+     * Instance var 'rate' getter method.
+     * @return rate
+     */
     public int getRate() {
         return rate;
     }
 
-    public int getBalance() {
+    /**
+     * Instance var 'cash' getter method.
+     * @return cash
+     */
+    public double getBalance() {
         return cash;
     }
 
+    /**
+     * Affects instance var 'cash' by calculating the owed interest and adding it to cash.
+     * @param time - Years passed
+     * @implNote newCash = cash * (1 + rate[.->%] / periods) * (periods * time)
+     */
     public void calcInt(int time) {
-        cash = ((rate / 100) * time + 1);
+        cash *= Math.pow((1.0 + ((rate / 100.0) / 12.0)), (time * 12));
     }
 
 }
