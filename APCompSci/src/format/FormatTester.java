2ede9b2487934849445724f0d4bb179683ada9da
hammk <67597830+hammk@users.noreply.github.com>
2022-11-30 09:26:07 -0600

    Project finished.

--- APCompSci/src/format/FormatTester.java
+++ APCompSci/src/format/FormatTester.java
@@ -22,20 +22,20 @@ public class FormatTester
     
              ArrayList<String>words2 = new ArrayList<String>();
              words2.add("I ever tell you");
-             words2.add("About the time my");
-             words2.add("Buddy Keith fell out");
-             words2.add("The rollercoaster? Yeah, he");
-             words2.add("Didn't drop far, mind");
-             words2.add("You, just onto the");
-             words2.add("Tracks, but the carnival");
-             words2.add("People wouldn't stop the");
-             words2.add("Ride 'cause all the");
-             words2.add("Other people paid good");
-             words2.add("Money and Keith snuck");
-             words2.add("On for free, so");
-             words2.add("He had to dodge");
-             words2.add("For, like, 20 minutes");
-             words2.add("Or so.");
-             System.out.println("Should print 285 and prints: " + Format.sumOfLetters(words2));
+             words2.add("about the time my");
+             words2.add("buddy Keith fell out");
+             words2.add("the rollercoaster? Yeah, he");
+             words2.add("didn't drop far, mind");
+             words2.add("you, just onto the");
+             words2.add("tracks, but the carnival");
+             words2.add("people wouldn't stop the");
+             words2.add("ride 'cause all the");
+             words2.add("other people paid good");
+             words2.add("money and Keith snuck");
+             words2.add("on for free, so");
+             words2.add("he had to dodge");
+             words2.add("for, like, 20 minutes");
+             words2.add("or so.");
+             System.out.println("Should print 285 and prints: " + Format.sumOfLetters(words2) + "\n" + words2.toString());
         }
 }
