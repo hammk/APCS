1b38da3b45aa6822fb69a55667671ea08e709c14
hammk <67597830+hammk@users.noreply.github.com>
2022-12-05 09:45:56 -0600

    javadoc finished

--- APCompSci/src/large2D/TDTest.java
+++ APCompSci/src/large2D/TDTest.java
@@ -4,7 +4,7 @@ public class TDTest {
 
     public static void main(String[] args) {
 
-        int[][] gvn = {{34,12,3},{500,21,4},{13,9,2},{12,11,334}};
+        int[][] gvn = {{34,12,3},{42,21,4},{13,9,2},{12,11,334}};
         
         System.out.println(TwoDArray.getLargestValue(gvn));
 
--- APCompSci/src/large2D/TwoDArray.java
+++ APCompSci/src/large2D/TwoDArray.java
@@ -1,23 +1,44 @@
 package large2D;
 
+/**
+ * This class passes through a 2D array to determine the highest value and it's index.
+ * @author humzahkha0143
+ */
 public class TwoDArray {
     
+    /**
+     * This single method initializes variables for the indexes of X and Y
+     * across the array and what so far it thinks is the highest number, then
+     * it traverses the given 2D array. When it finds a number higher than
+     * it's own 'highest' number, that number and it's index are stored.
+     * When it finishes the array, those two values are returned.
+     * @param ary - Two-Dimensional Array to search.
+     * @return num - Highest detected number.
+     * @return xC - X-axis coordinates for num.
+     * @return yC - Y-axis coordinates for num.
+     */
     public static String getLargestValue(int[][] ary) {
         
         int xIndex = 0;
+        int xC = 0;
         int yIndex = 0;
+        int yC = 0;
         int num = 0;
         
         for(xIndex = 0; xIndex < ary.length; xIndex++) {
         
             for(yIndex = 0; yIndex < ary[yIndex].length; yIndex++) {
                 
-                if(num < ary[xIndex][yIndex]) num = ary[xIndex][yIndex];
+                if(num < ary[xIndex][yIndex]) {
+                    num = ary[xIndex][yIndex];
+                    xC = xIndex;
+                    yC = yIndex;
+                }
                 
             }
         
         }
-        return "Largest Value: " + num + ", Index: (" + (xIndex - 1) + ", " + (yIndex - 1) + ")";
+        return "Largest Value: " + num + ", Index: (" + xC + ", " + yC + ")";
         
     }
     
